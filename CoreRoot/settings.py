164087ab0079748ec111372396cccf34af1e7d4f
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-02-19 16:59:49 +0300

    delete dbsqlite3 and migrate user_core

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'core.user',
+     
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+AUTH_USER_MODEL = 'core_user.User'
