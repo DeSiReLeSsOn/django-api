ad923d50c6e1b0e4b6adeb7973c71acd8d870a8d
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-02-22 18:39:43 +0300

    add LoginViewSet in routers.py

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets.register import RegisterViewSet, LoginViewSet
 
 
 
@@ -20,6 +20,7 @@ router.register(r'user', UserViewSet, basename='user')
 # ##################################################################### #
 
 router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 
 urlpatterns = [
