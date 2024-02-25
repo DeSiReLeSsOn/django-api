3de2afbc8773f8281b06aef57080264b9812c481
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-02-25 17:57:05 +0300

    refactor User class and UserManager

--- core/user/models.py
+++ core/user/models.py
@@ -1,22 +1,18 @@
-import uuid
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-from django.http import Http404 
+
+from core.abstract.models import AbstractModel, AbstractManager
 
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return "user_{0}/{1}".format(instance.public_id, filename)
 
-class UserManager(BaseUserManager):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except(ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
-        
+class UserManager(BaseUserManager, AbstractManager):
 
     def create_user(self, username, email, password=None, **kwargs):
         """Create and return a `User` with an email, phone
@@ -56,9 +52,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True,
-    default=uuid.uuid4, editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True,
     max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
@@ -66,8 +60,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, blank=True, upload_to=user_directory_path)
     
