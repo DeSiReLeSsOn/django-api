6c98b34afbefd0cdf2f4ea5823218f8cc8e4a815
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-02-26 17:13:03 +0300

    updated user viewset

--- core/abstract/viewsets.py
+++ core/abstract/viewsets.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from rest_framework import filters
+from core.abstract.viewsets import AbstractViewSet
+from core.user.serializers import UserSerializer
+from core.user.models import User
 
 
-class AbstractViewSet(viewsets.ModelViewSet):
+class UserViewSet(AbstractViewSet):
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['updated', 'created']
     ordering = ['-updated']
