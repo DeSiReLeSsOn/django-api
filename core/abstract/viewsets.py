48ebc6178985de034ae1a0f2fd1f29f86ffb2b75
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-02-27 19:13:43 +0300

    created serializers.py in post app

--- core/abstract/viewsets.py
+++ core/abstract/viewsets.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets
 from rest_framework import filters
-from core.abstract.viewsets import AbstractViewSet
 from core.user.serializers import UserSerializer
 from core.user.models import User
 
 
-class UserViewSet(AbstractViewSet):
+class AbstractViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['updated', 'created']
     ordering = ['-updated']
