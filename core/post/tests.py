030a92206abc360f7930530d26656a76be964132
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-11 15:55:46 +0300

    add base test for post_create

--- core/post/tests.py
+++ core/post/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from core.fixtures.user import user
+from core.post.models import Post
+import pytest
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user, body="Test Post Body")
+    assert post.body == "Test Post Body"
+    assert post.author == user
