304d921a502d3181e757fe6bf6f924e869fac75b
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-12 15:25:48 +0300

    test client_register

--- core/auth/tests.py
+++ core/auth/tests.py
@@ -20,3 +20,17 @@ class TestAuthencitionViewSet:
         assert response.data['user']['id'] == user.public_id.hex
         assert response.data['user']['username'] == user.username
         assert response.data['user']['email'] == user.email
+
+
+@pytest.mark.django_db
+def test_register(self, client):
+    data = {
+        "username": "johndoe",
+        "email": "johndoe@yopmail.com",
+        "password": "test_password",
+        "first_name": "John",
+        "last_name": "Doe"
+    }
+    response = client.post(self.endpoint + "register/", data)
+    assert response.status_code == status.HTTP_201_CREATED
+    
