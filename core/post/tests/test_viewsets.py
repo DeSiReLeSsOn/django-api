dee18bee68a0e22deee43b88589c6b529c233f83
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-14 17:19:55 +0300

    created test for get_user,client ,post

--- core/post/tests/test_viewsets.py
+++ core/post/tests/test_viewsets.py
@@ -14,6 +14,15 @@ class TestPostViewSet:
         assert response.data["count"] == 1
 
 
+    def test_retrieve(self, client, user, post):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint + str(post.public_id) + "/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == post.public_id.hex 
+        assert response.data['body'] == post.body
+        assert response.data['author']['id'] == post.author.public_id.hex
+
+
 
     def test_create(self, client, user):
         client.force_authenticate(user=user)
