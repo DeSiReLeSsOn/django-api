a18d26e3f91d10391a3387a44014cbc1e302c002
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-14 17:08:35 +0300

    created test for comment_create method

--- core/post/tests/test_viewsets.py
+++ core/post/tests/test_viewsets.py
@@ -12,3 +12,16 @@ class TestPostViewSet:
         response = client.get(self.endpoint)
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 1
+
+
+
+    def test_create(self, client, user):
+        client.force_authenticate(user=user)
+        data = {
+            "body": "Test Post Body",
+            "author": user.public_id.hex
+        }
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['body'] == data['body']
+        assert response.data['author']['id'] == user.public_id.hex
