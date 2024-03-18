3917bb2a3ad06037e4cc535a2774d410ed548e2e
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-18 15:09:55 +0300

    create test for user permissions to crud oparations

--- core/user/tests/test_viewsets.py
+++ core/user/tests/test_viewsets.py
@@ -24,7 +24,17 @@ class TestUserViewSet:
 
 
     def test_create(self, client, user):
-        pass
+        client.force_authenticate(user=user)
+        data = {}
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
     def test_update(self, client, user):
-        pass
+        client.force_authenticate(user=user)
+        data = {
+            "username": "test_user_updated",
+        }
+
+        response = client.patch(self.endpoint + str(user.public_id) + "/", data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['username'] == data['username']
