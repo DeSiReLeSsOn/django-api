e85dc1d9b58e1f381397362b790e90509d9a4ccb
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-25 19:38:11 +0300

    write the basic configurations, such as the URL and some headers

--- social-media-app/src/helpers/axios.js
+++ social-media-app/src/helpers/axios.js
@@ -2,9 +2,20 @@ import axios from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
 
 
-const axiousService = axios.create({
+const axiosService = axios.create({
     baseURL: "http://localhost:8000", 
     headers: {
         "Content-Type": "application/json",
     },
+});
+
+
+axios.Service.interceptors.request.use(async (config) => {
+    /**
+     * Retrieving the access token from the localStorage
+    and adding it to the headers of the request
+    */
+const { access } = JSON.parse.apply(localStorage.getItem("auth"));
+config.headers.Authorization = `Bearer ${access}`;
+return config;
 });
