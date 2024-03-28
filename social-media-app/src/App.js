8f54452db9bc7038ed811796bb54af1e4d9fc6ea
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-28 18:45:20 +0300

    the default location will be the profile page

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
 import Home from "./pages/Home";
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<ProtectedRoute>
+        <Home />
+      </ProtectedRoute> } />
+      <Route path="/login/" element={<div>Login</div>} />
     </Routes>
   );
 }
