b0ca2a22866d8d1599c0a42351c40223b6e8c1f8
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-28 19:00:55 +0300

    write the function that will handle the form submission

--- social-media-app/src/components/authentication/RegistrationForm.jsx
+++ social-media-app/src/components/authentication/RegistrationForm.jsx
@@ -10,3 +10,24 @@ function RegistrationForm() {
     const [form, setForm] = useState({});
     const [error, setError] = useState(null);
 }
+
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const registrationForm = event.currentTarget;
+
+    if (registrationForm.checkValidity() === false) {
+        event.stopPropagation();
+    }
+
+    setValidated(true);
+
+    const data = {
+        username: form.username,
+        password: form.password,
+        email: form.email,
+        first_name: form.first_name, 
+        last_name: form.last_name,
+        bio: form.bio,
+    };
+}
