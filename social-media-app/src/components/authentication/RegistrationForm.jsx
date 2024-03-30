1c1f1065c9323e41cf7f7a37187781cfd5bf3e35
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-03-30 18:03:32 +0300

    create form.control who can take another fields as props

--- social-media-app/src/components/authentication/RegistrationForm.jsx
+++ social-media-app/src/components/authentication/RegistrationForm.jsx
@@ -70,5 +70,77 @@ const handleSubmit = (event) => {
                   This file is required.
                 </Form.Control.Feedback>
               </Form.Group>
-    ) ;  
-}
+              <Form.Group className="mb-3">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  value={form.last_name}
+                  onChange={(e) => setForm({ ...form, last_name: e.target.value })}
+                  required
+                  type="text"
+                  placeholder="Enter last name"
+                />
+                <Form.Control.Feedback type="invalid">
+                  This file is required.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  value={form.username}
+                  onChange={(e) => setForm({ ...form, username: e.target.value })}
+                  required
+                  type="text"
+                  placeholder="Enter username"
+                />
+                <Form.Control.Feedback type="invalid">
+                  This file is required.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  value={form.email}
+                  onChange={(e) => setForm({ ...form, email: e.target.value })}
+                  required
+                  type="email"
+                  placeholder="Enter email"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid email.
+                </Form.Control.Feedback>
+              </Form.Group>
+        
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={form.password}
+                  minLength="8"
+                  onChange={(e) => setForm({ ...form, password: e.target.value })}
+                  required
+                  type="password"
+                  placeholder="Password"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid password.
+                </Form.Control.Feedback>
+              </Form.Group>
+        
+              <Form.Group className="mb-3">
+                <Form.Label>Bio</Form.Label>
+                <Form.Control
+                  value={form.bio}
+                  onChange={(e) => setForm({ ...form, bio: e.target.value })}
+                  as="textarea"
+                  rows={3}
+                  placeholder="A simple bio ... (Optional)"
+                />
+              </Form.Group>
+        
+              <div className="text-content text-danger">{error && <p>{error}</p>}</div>
+        
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          );
+        }
