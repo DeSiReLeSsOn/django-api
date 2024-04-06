74db4f557c5890dbc9032d12cf0b41bee684d5d8
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-04-06 19:33:00 +0300

    add createpost func

---
+++ social-media-app/src/components/posts/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import axiosServise from "../../helpers/axios";
+import { getUser } from "../../hooks/user.actions";
+
+
+function CreatePost() {
+    return ()
+};
+
+
+export default CreatePost;
