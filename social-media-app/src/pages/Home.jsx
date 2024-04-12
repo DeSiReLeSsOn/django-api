1c1324c7b31cf2f85f03f09e845552b544297054
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-04-12 16:30:04 +0300

    create a post.jsx

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -5,35 +5,47 @@ import { randomAvatar } from "../utils";
 import useSWR from "swr";
 import { fetcher } from "../helpers/axios";
 import { getUser } from "../hooks/user.actions";
+import  Post  from "../components/posts/Post";
 import CreatePost from "../components/posts/CreatePost";
 
-
 function Home() {
-    const user = getUser();
+  const posts = useSWR("/post/", fetcher, {
+    refreshInterval: 20000,
+  });
 
-    if (!user) {
-        return <div>Loading!</div>;
-    }
-    return (
-        <Layout>
-            <Col sm={7}>
-                <Row className="border rounded align-items-center">
-                    <Col className="flex-shrink-1">
-                        <Image src={randomAvatar()}
-                        roundedCircle
-                        width={52}
-                        height={52}
-                        className="my-2"
-                        />
-                    </Col>
-                    <Col sm={10} className="flex-grow-1">
-                        <CreatePost />
-                    </Col>
-                </Row>
+  const user = getUser();
+
+  if (!user) {
+    return <div>Loading!</div>;
+  }
+
+  return (
+    <Layout>
+      <Row className="justify-content-evenly">
+        <Col sm={7}>
+          <Row className="border rounded  align-items-center">
+            <Col className="flex-shrink-1">
+              <Image
+                src={randomAvatar()}
+                roundedCircle
+                width={52}
+                height={52}
+                className="my-2"
+              />
             </Col>
-        </Layout>
-    );
+            <Col sm={10} className="flex-grow-1">
+              <CreatePost refresh={posts.mutate} />
+            </Col>
+          </Row>
+          <Row className="my-4">
+            {posts.data?.results.map((post, index) => (
+              <Post key={index} post={post} refresh={posts.mutate} />
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Layout>
+  );
 }
-
 
 export default Home;
