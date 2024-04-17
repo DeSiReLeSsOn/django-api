6a55fcaa0a5ca7d5ca6189ba883dbfd07686182c
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-04-17 17:04:39 +0300

    create SinglePost func

--- social-media-app/src/pages/SinglePost.jsx
+++ social-media-app/src/pages/SinglePost.jsx
@@ -7,3 +7,28 @@ import { fetcher } from "../helpers/axios";
 import { Post } from "../components/posts";
 import CreateComment from "../components/comments/CreateComment";
 import Comment from "../components/comments/Comment";
+
+
+function SinglePost() {
+    let { postId } = useParams();
+
+    const post = useSWR(`/post/${postId}/`, fetcher);
+
+    const comments = useSWR(`/post/${postId}/comment/`, fetcher);
+
+    return (
+        <Layout hasNavigationBack>
+            {post.data ? (
+                <Row className="justify-content-center">
+                    <Col sm={8}>
+                        <Post post={post.data} refresh={post.mutate} 
+                            isSinglePost />
+                        // Adding CreateComment form and list all comments here
+                    </Col>
+                </Row>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </Layout>
+    );
+}
