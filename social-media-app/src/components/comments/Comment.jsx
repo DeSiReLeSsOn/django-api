b8a7b6ffd543f5143fb4468376a56b49db2c2350
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-04-22 19:46:59 +0300

    add comment ui in singlepost view

---
+++ social-media-app/src/components/comments/Comment.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useContext } from "react";
+import { format } from "timeago.js";
+import { Image, Card, Dropdown } from "react-bootstrap";
+import { randomAvatar } from "../../utils";
+import axiosService from "../../helpers/axios";
+import { getUser } from "../../hooks/user.actions";
+import UpdateComment from "./UpdateComment";
+import { Context } from "../Layout";
+import MoreToggleIcon from "../MoreToggleIcon";
+
+function Comment(props) {
+    const { postId, comment, refresh } = props;
+    const { toaster, setToaster } = useContext(Context); 
+
+    const user = getUser();
+
+    const handleDelete = () => {
+        // Handle the deletion of a comment
+    };
+
+    return (
+        <Card className="rounded-3 my-2">
+            <Card.Body>
+                <Card.Title className="d-flex flex-row justify-content-between">
+                    <div className="d-flex flex-row">
+                        <Image src={randomAvatar()}
+                            roundedCircle
+                            width={48}
+                            height={48}
+                            className="me-2 border border-primary border-2"
+                        />
+                        <div className="d-flex flex-column justify-content-start align-self-center mt-2">
+                            <p className="fs-6 m-o">{comment.author.name}</p>
+                            <p className="fs-6 fw-lighter">
+                                <small>{format(comment.created)}</small>
+                            </p>
+                        </div>
+                    </div>
+                    {user.name === comment.author.name && (
+                        <div>
+                            <Dropdown>
+                                <Dropdown.Toggle
+                                    as={MoreToggleIcon}></Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item>
+                                        Modify 
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                                <Dropdown.Item onClick={handleDelete}
+                                    className="text-danger">
+                                        Delete
+                                    </Dropdown.Item>
+                            </Dropdown>
+                        </div>
+                    )}
+                </Card.Title>
+                <Card.Text>{comment.body}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Comment;
