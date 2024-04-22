377c66858f2a21f834cbef4860dbf503af3db86a
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-04-22 19:11:14 +0300

    add handler of comment

--- social-media-app/src/components/comments/CreateComment.jsx
+++ social-media-app/src/components/comments/CreateComment.jsx
@@ -17,7 +17,39 @@ function CreateComment(props) {
     const user = getUser();
 
     const handleSubmit = (event) => {
-        
+        event.preventDefault();
+        const createCommentForm = event.currentTarget;
+
+        if (createCommentForm.checkValidity() === false) {
+            event.stopPropagation();
+        }
+        setValidated(true);
+
+        const data = {
+            author: user.id, 
+            body: form.body,
+            post: postId,
+        };
+        axiosService
+            .post(`/post/${postId}/comment/`, data)
+            .then(() => {
+                setForm({ ...form, body: "" });
+                setToaster({
+                    type: "success",
+                    message: "Comment posted successfully🚀",
+                    show: true,
+                    title: "Comment!",
+                });
+                refresh();
+            })
+            .catch(() => {
+                setToaster({
+                    type: "danger",
+                    message: "", 
+                    show: true, 
+                    title: "An error occured.!",
+                });
+            });
     };
 
 
